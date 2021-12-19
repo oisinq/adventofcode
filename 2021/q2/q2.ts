815caea7f1d56008073413380d19ec1424fd0b6a
Oisín Quinn <oisin1001@gmail.com>
2021-12-19 00:22:02 +0000

    Completed 2021/q2

---
+++ 2021/q2/q2.ts
@@ -0,0 +1,65 @@
+import * as fs from "fs";
+import * as path from "path";
+import { printResult } from "../print";
+
+type Instruction = {
+  direction: "forward" | "up" | "down";
+  amount: number;
+};
+
+const read = fs.readFileSync(path.resolve(__dirname, "input.txt"));
+const instructions = read
+  .toString()
+  .split("\n")
+  .map((line) => line.split(" "))
+  .map(([direction, amount]) => ({
+    direction,
+    amount: Number(amount),
+  })) as Instruction[];
+
+const part1 = () => {
+  let horizontalPostion = 0;
+  let depth = 0;
+
+  for (const instruction of instructions) {
+    switch (instruction.direction) {
+      case "forward":
+        horizontalPostion += instruction.amount;
+        break;
+      case "up":
+        depth -= instruction.amount;
+        break;
+      case "down":
+        depth += instruction.amount;
+        break;
+    }
+  }
+
+  printResult({ part: 1, answer: depth * horizontalPostion });
+};
+
+const part2 = () => {
+  let horizontalPostion = 0;
+  let depth = 0;
+  let aim = 0;
+
+  for (const instruction of instructions) {
+    switch (instruction.direction) {
+      case "forward":
+        horizontalPostion += instruction.amount;
+        depth += aim * instruction.amount;
+        break;
+      case "up":
+        aim -= instruction.amount;
+        break;
+      case "down":
+        aim += instruction.amount;
+        break;
+    }
+  }
+
+  printResult({ part: 2, answer: depth * horizontalPostion });
+};
+
+part1();
+part2();
